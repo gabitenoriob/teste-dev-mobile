aa4895abd8720152dd29ef9b05b05b8e7c50b344
gabitenoriob <gabitenoriob@gmail.com>
2023-11-07 23:34:52 -0300

    tela inicial criada

--- README.md
+++
@@ -1,2 +0,0 @@
-# teste-dev-mobile-doity
-Teste de um processo seletivo que consiste em fazer o front-end de um app mobile de um evento consumindo a API da empresa
---
+++ app_evento/lib/src/widgets/my_app.dart
@@ -0,0 +1,17 @@
+import 'package:app_evento/src/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Meu App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
